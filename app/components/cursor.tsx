4e1ddb368bcea90b90b1e4300f147b6fe1ad06eb
IsaacM1210 <1210Mhamed@gmail.com>
2024-02-15 10:23:24 -0700

    Cursor opacity changes over clickable elements

--- app/components/cursor.tsx
+++ app/components/cursor.tsx
@@ -1,17 +1,63 @@
 "use client";
 
 import { useMouse } from "@/hooks/mouse-state";
+import { MouseCoords } from "@/types/position";
+import { useState, useEffect } from "react";
+
+const CURSOR_SPEED = 0.05;
 
 export default function Cursor() {
-    const { position } = useMouse();
+    const { position, canClick } = useMouse();
+    const [ CursorPosition, setCursorPosition ] = useState<MouseCoords>({
+        x: 0,
+        y: 0,
+      });
+
+
+    useEffect(() => {
+        let frame: number;
+    
+        const animate = () => {
+          frame = requestAnimationFrame(animate);
+    
+          setCursorPosition((previous) => {
+            const distance: MouseCoords = {
+              x: (position.x - previous.x) * CURSOR_SPEED,
+              y: (position.y - previous.y) * CURSOR_SPEED,
+            };
+    
+            const newCursorPosition = {
+              x: Math.floor((previous.x + distance.x) * 10) / 10,
+              y: Math.floor((previous.y + distance.y) * 10) / 10,
+            };
+    
+            if (
+              newCursorPosition.x === previous.x &&
+              newCursorPosition.y === previous.y
+            )
+              return previous;
+    
+            return newCursorPosition;
+          });
+        };
+    
+        animate();
+        return () => cancelAnimationFrame(frame);
+      }, [position]);
 
     return (
+        <>
         <div style={{
-            left: `${position.x}px`,
-            top: `${position.y}px`
+            left: `${CursorPosition.x}px`,
+            top: `${CursorPosition.y}px`,
+            opacity: canClick ? ".2" : "1"
 
         }}
-            className="fixed z-50 transition will-change-transform ease-out -translate-x-1/2 -translate-y-1/2 duration-100 pointer-events-none h-6 w-6 border rounded-full border-white">
+            className="fixed z-50 transition will-change-transform ease-out -translate-x-1/2 -translate-y-1/2 duration-100 pointer-events-none h-6 w-6 border rounded-full border-white flex justify-evenly items-center">
+                <div className=" border h-1 w-1 rounded-full border-white bg-white">
+                </div>
             </div>
+            
+    </>
     )
 }
--- hooks/mouse-state.tsx
+++ hooks/mouse-state.tsx
@@ -1,7 +1,46 @@
 import { MouseCoords } from "@/types/position";
 import { useState, useEffect } from "react";
+
+const CLICKABLE_HTML = ["BUTTON", "A"];
+
 type MouseHoverEvent = MouseEvent & { path: HTMLCollection };
 
+/**
+ * return a boolean value based on whether the event target is a clickable element defined in the constant CLICKABLE_HTML
+ * @param target 
+ * @returns boolean value
+ */
+const isClickable = (target: EventTarget) => {
+    if(target instanceof HTMLElement){
+        return CLICKABLE_HTML.includes(target.tagName);
+    }
+    return false
+}
+
+/**
+ * A hook that will return a boolean based on whether the element the cursor is
+ *  over is a CLICKABLE_HTML
+ * @returns Boolean 
+ */
+const useCanClick = () => {
+    const [ canClick, setCanClick ] = useState<boolean>(false);
+
+    useEffect(() => {
+        const handleMouseMove = ((event: MouseHoverEvent) => {
+            const containsClickable = Array.from(event.composedPath()).some(isClickable);
+            setCanClick(containsClickable);
+        }) as (event: MouseEvent) => void;
+
+        window.addEventListener("mousemove", handleMouseMove);
+        return () => window.addEventListener("mousemove", handleMouseMove);
+    });
+    return {canClick}
+};
+
+/** 
+ * a hook that returns the current position of the cursor
+ * @returns a value of type MouseCoords
+ */ 
 const useMousePosition = () => {
     const [position, setPosition] = useState<MouseCoords>({ x: 0, y: 0 });
 
@@ -17,9 +56,13 @@ const useMousePosition = () => {
     return { position };
 };
 
-
+/**
+ * a hoot that returns objects containting cursor data
+ * @returns an object containing cursor position, if it can click
+ */
 export const useMouse = () => {
     const { position } = useMousePosition();
+    const { canClick } = useCanClick();
 
-    return { position };
+    return { position, canClick };
 }
